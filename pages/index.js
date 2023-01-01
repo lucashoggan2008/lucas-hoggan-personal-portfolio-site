19f6e701fa3a1ecb1b501c525f49874f12e29686
lucashoggan2008 <77905024+lucashoggan2008@users.noreply.github.com>
2023-01-01 20:29:11 +0000

    v1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
+/*
+<nav>
+          <h1 className="title-text">Lucas Hoggan</h1>
+        </nav>
+*/
 
+const WelcomeContainer = () => {
+  const handleClickScroll = () => {
+    const element = document.getElementById("first-section")
+    if (element) {
+      element.scrollIntoView({ behavior:"smooth"})
+    }
+  }
+
+  return (
+    <div className="welcome-container">
+          <h1 className="title-text">HELLO.</h1>
+            <button id="scroll-btn" onClick={handleClickScroll}>
+              <h2 className="sub-text">I am Lucas Hoggan</h2>
+            </button>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
@@ -12,14 +34,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="main">
-        <nav>
-          <h1 className="title-text">Lucas Hoggan</h1>
-        </nav>
-        <div className="section">
+        <WelcomeContainer />
+        <div id="first-section" className="section">
           <h2 className="header">Who i am.</h2>
           <div className="divider"></div>
           <div className="row">
-            <p className="paragraph">Hi there! My name is Lucas and I am a 14 year old freelance developer. In my free time, I enjoy staying active and healthy by playing rugby and hitting the gym. I am passionate about technology and love using my skills to create and build new projects. I am always looking for new opportunities to challenge myself and expand my knowledge in the field of development, I am based in central Scotland. Thank you for visiting my website</p>
+            <p className="paragraph">Hi there! My name is Lucas and I am a 14 year old <em>freelance developer</em>. In my free time, I enjoy staying active and healthy by playing rugby and hitting the gym. I am passionate about technology and love using my skills to create and build new projects. I am <em>always</em> looking for new opportunities to challenge myself and expand my knowledge in the field of development, I am based in central Scotland. <em>Thank you</em> for visiting my website</p>
             <Image id="home-page-good-photo-1" src="/good-photo-1.jpg" width="1000" height="1000" alt="Picture of me"/>
           </div>
         </div>
