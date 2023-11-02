ddb567c7e713216a4a4dd1cd673f7323637f79f2
lucashoggan2008 <77905024+lucashoggan2008@users.noreply.github.com>
2023-11-02 14:34:19 +0000

    Remove main page content

--- pages/index.js
+++ pages/index.js
@@ -17,18 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="main">
-        <Navbar />
-        <div className="main-section">
-          <Banner title="HOME" />
-          <div className="spacing"></div>
-          <section>
-            <h1 className="title">Who I am.</h1>
-            <p>I am an up-and-coming web developer and junior powerlifter from Fife, Scotland. I have been in the website development space for about 3 years and I am trying now to help businesses grow and create a better image with professional website development, hosting and management services.</p>
-          </section>
-          <Footer />
-        </div>
-      </main>
+     
     </>
   )
 }
