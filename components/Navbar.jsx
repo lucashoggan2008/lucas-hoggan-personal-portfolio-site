d4ec833f431067558b4c1837c827c428520f4540
lucashoggan2008 <77905024+lucashoggan2008@users.noreply.github.com>
2023-11-11 21:32:00 +0000

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <header className="bg-pallete-olive w-screen h-[5rem] flex justify-between items-center overflow-hidden pl-2">
       <Link className='p-0 h-[5rem]' href='/'>
-        <PhoneWebComponent PhoneComponent={<Image width={0} height={0}  className="w-[5rem] h-[5rem]" src="/icon-logo.svg" alt="logo" />} WebComponent={<h1 className=' text-pallete-flame text-3xl  font-bold rounded-sm shadow-pallete-floral'><span className='text-pallete-floral'>Lucas /</span>/ Hoggan</h1>} />
+        <PhoneWebComponent PhoneComponent={<Image width={0} height={0} rel="noreferrer"  className="w-[5rem] h-[5rem]" src="/icon-logo.svg" alt="logo" />} WebComponent={<h1 className=' text-pallete-flame text-3xl  font-bold rounded-sm shadow-pallete-floral'><span className='text-pallete-floral'>Lucas /</span>/ Hoggan</h1>} />
       </Link>
     </header>
   )
--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ const MainPage = () => {
   return (
     <main>
       <Seperator />
-      <PhoneWebComponent PhoneComponent={<Image alt="about me"   width={0} height={0} className="w-screen" src="/intro-code-banner.svg" />} WebComponent={<Image alt="about me"   width={0} height={0}  className="w-screen" src="/intoduction-banner.svg" />} />
+      <PhoneWebComponent PhoneComponent={<Image rel="noreferrer" alt="about me"   width={0} height={0} className="w-screen" src="/intro-code-banner.svg" />} WebComponent={<Image rel="noreferrer" alt="about me"   width={0} height={0}  className="w-screen" src="/intoduction-banner.svg" />} />
       <Seperator />
-      <PhoneWebComponent PhoneComponent={<Image priority={true} alt="photo of me"   width={0} height={0} className="w-screen" src="/phone-lift-collage.jpg" />} WebComponent={<Image priority={true} alt="photo of me" className="w-screen"   width={0} height={0}  src="/web-lift-collage.jpg" />}  />
+      <PhoneWebComponent PhoneComponent={<Image rel="noreferrer" priority={true} alt="photo of me"   width={0} height={0} className="w-screen" src="/phone-lift-collage.jpg" />} WebComponent={<Image rel="noreferrer" priority={true} alt="photo of me" className="w-screen"   width={0} height={0}  src="/web-lift-collage.jpg" />}  />
       {/*<Parallax pages = {5}>
         <ParallaxLayer offset={0} sticky={{start:0, end:5}}>
           <div className="w-[70vw] h-[100vw] bg-pallete-olive"></div>
